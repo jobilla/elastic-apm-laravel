99f1982128bfc89001a58f4c1440b7c593c4227b
Leo Sjöberg <leo.sjoberg@gmail.com>
2020-02-21 21:59:59 +0200

    fix query

--- src/Providers/ElasticApmServiceProvider.php
+++ src/Providers/ElasticApmServiceProvider.php
@@ -186,7 +186,7 @@ class ElasticApmServiceProvider extends ServiceProvider
 
             $tempParent = new EventBean([]);
             $tempParent->setTraceId('123');
-            $span = new CompletedSpan('Eloquent Query', $tempParent);
+            $span = new CompletedSpan('Eloquent Query', $tempParent, $query->time);
             $span->setType('db.mysql.query');
             $span->setStacktrace($stackTrace->toArray());
             $span->setContext([
